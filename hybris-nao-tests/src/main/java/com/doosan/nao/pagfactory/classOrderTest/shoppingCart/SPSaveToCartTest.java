9b93c24f0473e62af9ef08e8b8152c36dbb6d971
rajkumars <rajkumars@Che-Lt-Rajak.corp.doosan.com>
2019-09-17 15:55:25 +0530

    Test update on new case

---
+++ hybris-nao-tests/src/main/java/com/doosan/nao/pagfactory/classOrderTest/shoppingCart/SPSaveToCartTest.java
@@ -0,0 +1,156 @@
+package com.doosan.nao.pagfactory.classOrderTest.shoppingCart;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.doosan.nao.PageFactory.HomePage;
+import com.doosan.nao.PageFactory.LoginPage;
+import com.doosan.nao.PageFactory.QuickOrderPage;
+import com.doosan.nao.PageFactory.SelectOrgPage;
+import com.doosan.nao.PageFactory.YourShoppingCartPage;
+import com.doosan.nao.constants.Constants;
+import com.doosan.nao.init.TestInitializer;
+import com.doosan.nao.init.WebdriverSelector;
+import com.doosan.nao.input.InputReader;
+import com.doosan.nao.pagfactory.classOrderTest.Testing;
+import com.doosan.nao.reports.ReportGenerator;
+
+public class SPSaveToCartTest extends TestInitializer {
+	private String browserName;
+	private String isBrowserAlive="N";
+	public EventFiringWebDriver wd=null;
+	private String localTestData;
+	public  String testRunId;
+	public String testCaseNumberID;
+	String path;
+	public String [] TCNum=null;
+	@Parameters(value = { "browser", "version", "platform", "osVersion" })
+	@BeforeMethod
+	public void setUpForSelectOrgPageTwoDropDownTest(String browser, String browserVersion, String platform, String osVersion) throws IOException, URISyntaxException, InterruptedException
+	{ 	System.out.println(" Verify the Shopping cart Page UI Objects ");
+		browserName=browser;
+		if (isBrowserAlive=="N")
+		{
+		wd=WebdriverSelector.getDriver(wd, browser);
+		
+		initialize(browser, browserVersion, platform, osVersion);
+		
+			wd.get(PROJECT.getProperty("DoosanPassportURL"));
+		}
+		QuickOrderPage quickOrderPage =new QuickOrderPage();
+		quickOrderPage.clearAllBrowserCache(wd);
+	}
+
+	
+	
+	@Test(priority=1,dataProvider="SPSaveToCartTest")
+	public void validateSaveToCartTest(String testData,String userName,String password, String brand, String custNo, String itemNum, String qty, String binLocation, String comments, String testUrlName,String testCaseNumber) throws InterruptedException
+	{	testRunId=PROJECT.getProperty("testRunID");
+		testCaseNumberID=testCaseNumber;
+		System.out.println("1350059");
+		TCNum=testCaseNumber.split(",");
+		for (String tcNum : TCNum) {
+			System.out.println("TestCase No ->"+tcNum);
+		}
+		
+		this.localTestData=testData;
+		
+		LoginPage loginPage=PageFactory.initElements(wd, LoginPage.class);
+		SelectOrgPage selectOrgPage=PageFactory.initElements(wd, SelectOrgPage.class);
+		HomePage homePage=PageFactory.initElements(wd, HomePage.class);
+		QuickOrderPage quickOrderPage=PageFactory.initElements(wd, QuickOrderPage.class);
+		YourShoppingCartPage yourShoppingCartPage=PageFactory.initElements(wd, YourShoppingCartPage.class);
+		if(isBrowserAlive=="N")
+		{
+			loginPage.enterUserName(userName.trim());
+		loginPage.enterPassword(password.trim());
+		loginPage.clickLoginButton();
+		Thread.sleep(10000L);
+		}
+		
+		wd.get(PROJECT.getProperty("DoosanParts5URL"));
+		//wd.get("http://parts3.qa.corp.doosan.com/");
+		if (!selectOrgPage.verifyWelcomeText(wd,userName.trim()))
+		{
+			System.out.println("i am in browser validation");
+			isBrowserAlive="N";
+			//wd.quit();
+		}
+		
+				
+		selectOrgPage.verifyFirstDropDownisDisplayed(wd);
+		selectOrgPage.getSelectedOptionFrmFirstDropDown(wd);
+		selectOrgPage.verifyBySecondDropDown(wd);		
+		selectOrgPage.searchByFirstDropdown("Brand");
+		selectOrgPage.selectFirstDropDownValue(wd, brand);
+		selectOrgPage.enterTextSecondContainsBox(custNo);
+		selectOrgPage.clickSearchButton();
+		selectOrgPage.clickFirstOrgLink(wd);
+		yourShoppingCartPage.verifyShoppingCartItemClearing(wd);
+		homePage.clickQuickOrderLink(wd);
+		quickOrderPage.isQuickOrderpageItemTableDisplay(wd);
+		quickOrderPage.enterItemNumer(itemNum);
+		quickOrderPage.enterQuantity(qty);
+		quickOrderPage.enterBinLocation(binLocation);
+		quickOrderPage.enterComments(comments);
+		String itNoQ=quickOrderPage.verifyItemNumisEntered(itemNum);
+		String qtyQ=quickOrderPage.verifyQuantityisEntered(qty);
+		String binQ=quickOrderPage.verifyBiLocationIsEntered(binLocation);
+		String CommQ=quickOrderPage.verifyCommentsIsEntered(comments);
+		quickOrderPage.clickaddToCart(wd);
+		yourShoppingCartPage.getItemfromShoppingGrid(wd);
+		
+		yourShoppingCartPage.getItemQtyfromShoppingGrid();
+		
+		yourShoppingCartPage.getItemBinfromShoppingGrid();
+		
+		yourShoppingCartPage.getItemCommfromShoppingGrid();
+		
+		yourShoppingCartPage.clicksaveToSavedCartsButton(wd);
+		yourShoppingCartPage.verifySaveCartHeaderText(wd);
+				
+	}
+		
+
+	
+	@DataProvider(name="SPSaveToCartTest")
+	public static Object[][] getDataForShipments() throws InterruptedException
+	{
+		//TestInitializer.isBrowserAlive="N";
+		return InputReader.getInputReader().getDataFromXLS("SPSaveToCartTest");
+		
+	}
+	
+	
+	@AfterMethod()
+	public void onTestFailure(ITestResult result) throws IOException
+	{int i=0;
+		if (result.isSuccess())
+		{
+			isBrowserAlive="A";
+		path=ReportGenerator.setLogAndCreateScreenshot("SPSaveToCartTest~"+browserName, Constants.DEFAULT_TESTNAME, localTestData, Constants.PASSED, wd);
+			//addResultForTestCase("4603", "1350059", 1,"Test Passed  "+i++ );+ className + "_" + testCaseName + "_" + testData + "_"+ status +
+			addResultForTestCase(testRunId,testCaseNumberID,1, "Save Cart  Test~ "+browserName+"__"+localTestData+"_"+Constants.PASSED+"\n"+"![](http://che-dt-idc1/Hybris/reports/Screenshots/"+path+")");
+			isBrowserAlive="N";																																			//che-dt-idc1/Hybris/reports/Screenshots\QuickOrderPageUIElementsTest~chrome__TestData2_passed.jpg
+			wd.quit();		
+		} else 
+		{//http://localhost/Hybris/Screenshots/
+			path=ReportGenerator.setLogAndCreateScreenshot("SPSaveToCartTest~"+browserName, Constants.DEFAULT_TESTNAME, localTestData, Constants.FAILED,result.getThrowable().toString(),wd);
+			addResultForTestCase(testRunId,testCaseNumberID,5, "Save cart  Test failed ~ "+browserName+"__"+localTestData+"_"+Constants.FAILED+"\n"+result.getThrowable()+"![](http://che-dt-idc1/Hybris/reports/Screenshots/"+path+")");
+			//addResultForTestCase("4603", "1350059", 5, "Test Failed");
+			System.out.println(result.getThrowable());
+			isBrowserAlive="N";
+			wd.quit();
+		}
+	}
+
+}
---
+++ hybris-nao-tests/src/test/java/com/doosan/nao/suite/Test.java
@@ -0,0 +1,12 @@
+package com.doosan.nao.suite;
+
+public class Test {
+	public static void main(String[] args) {
+		String sty = "";boolean f = false;
+		if("Your".contains("Your Shopping Cart")) {
+			f=true;
+		}
+		System.out.println(f);
+	}
+ 
+}
